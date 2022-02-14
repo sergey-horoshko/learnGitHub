14df5fe71d68f7d27b544dd8be9685469e402241
cultizm <Liks-ea@mail.ru>
2022-02-14 21:16:00 +0300

    homework 2

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-let money = prompt("Ваш бюджет на месяц", 40000);
-let time = prompt("Введите дату в формате YYYY-MM-DD", "1999-07-06");
-let expenses = {};
-
-let questionFirst = prompt(
-	"Введите обязательную статью расходов в этом месяце",
-	"Еда"
-);
-
-let questionFirstPrice = prompt("Во сколько обойдется?", "20000");
-
-let questionSecond = prompt(
-	"Введите обязательную статью расходов в этом месяце",
-	"Транспорт"
-);
-
-let questionSecondPrice = prompt("Во сколько обойдется?", "10000");
+let money = +prompt("Ваш бюджет на месяц?", ""),
+	time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
 	budget: money,
@@ -25,10 +10,34 @@ let appData = {
 	savings: false,
 };
 
-appData.expenses[questionFirst] = questionFirstPrice;
-appData.expenses[questionSecond] = questionSecondPrice;
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+		b = +prompt("Во сколько обойдется?", "");
 
-alert(appData.budget / 30);
+	if (
+		typeof a === "string" &&
+		(typeof a != null) & (typeof b != null) &&
+		a != "" &&
+		b != "" &&
+		a.length < 50
+	) {
+		console.log("done");
+		appData.expenses[a] = b;
+	} else {
+		i--;
+	}
+}
 
-console.log(appData);
-console.log(appData.expenses);
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+	console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+	console.log("Высокий уровень достатка");
+} else {
+	console.log("Произошла ошибка");
+}
